1fa9ff812f7027b5f6e1fc40479f88450904bf06
Harold Asiimwe <harryasiimwe@gmail.com>
2013-07-19 11:31:50 +0300

    Completed Signup

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 import re
 from .models import Account
 from django.core.exceptions import ObjectDoesNotExist
+from django.core.validators import validate_email
 
 
 class SignupForm(forms.Form):
@@ -17,21 +18,26 @@ class SignupForm(forms.Form):
         widget=forms.PasswordInput
         )
  
-def clean_passwordTwo(self):
-    if 'password1' in self.clean_data:
-        passwordOne = self.clean_data['passwordOne']
-        passwordTwo = self.clean_data['passwordTwo']
+    def clean_password(self):
+        if 'passwordOne' in self.cleaned_data:
+            passwordOne = self.cleaned_data['passwordOne']
+            passwordTwo = self.cleaned_data['passwordTwo']
 
-        if passwordOne == passwordTwo:
-            return passwordTwo
-    raise forms.ValidationError('Passwords do not match.')
+            if passwordOne == passwordTwo:
+                return passwordTwo
+        raise forms.ValidationError('Passwords do not match.')
 
-def clean_username(self):
-    username = self.clean_data['username']
-    if not re.search(r'^\w+$', username):
-        raise forms.ValidationError('Username can only contain alphanumeric Characters and the underscore.')
-    try:
-        Account.objects.get(username=username)
-    except ObjectDoesNotExist:
-        return username
-    raise forms.ValidationError('Username is already Taken')
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if not re.search(r'^\w+$', username):
+            raise forms.ValidationError('Username can only contain alphanumeric Characters and the underscore.')
+        try:
+            Account.objects.get(username=username)
+        except ObjectDoesNotExist:
+            return username
+        raise forms.ValidationError('Username is already Taken')
+
+    def validate(self, value):
+        super(SignupForm, self).validate(value)
+        for email in value:
+            validate_email(email)
